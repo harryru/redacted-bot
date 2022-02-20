8d1ce8b40ea88fb693e461a76998e947859e401e
Harry Rubenzahl <me@harry.im>
2022-02-20 17:06:17 -0400

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,5 +1,5 @@
-const Discord = require("discord.js"); //Library
-const config = require("./config.json"); //Loading config file
+const Discord = require("discord.js");
+const config = require("./config.json");
 const client = new Discord.Client({ intents: [
     Discord.Intents.FLAGS.GUILDS,
     Discord.Intents.FLAGS.GUILD_MEMBERS
