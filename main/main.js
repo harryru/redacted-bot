c11be5ca3997f8ca60937b27f95a035b89085dd4
Harry Rubenzahl <me@harry.im>
2022-02-20 17:04:56 -0400

    Initial Commit
    Base bot with auto role feature.

---
+++ main/main.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js"); //Library
+const config = require("./config.json"); //Loading config file
+const client = new Discord.Client({ intents: [
+    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MEMBERS
+] });
+
+client.on('ready', () => {
+    console.log(`Launched as a bot: ${client.user.tag}!`);
+});
+
+/* If user joins server, check if we're looking at the right server, then auto grant "Friends" role. */
+client.on('guildMemberAdd', member => { //User joins server
+    if(client.guilds.cache.get(config.SERVER_ID)) {
+        return member.roles.add(member.guild.roles.cache.get(config.ROLE_ID));
+    }
+});
+
+client.login(config.BOT_TOKEN);
