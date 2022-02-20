b6e4a9f7440bc098ee4df2194b6e2141ce1d9d3c
Edgar Patino <edgar.patino.d@gmail.com>
2022-02-20 15:57:54 -0600

    add member count functionality

--- main/main.js
+++ main/main.js
@@ -1,21 +1,32 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
-const client = new Discord.Client({ intents: [
-    Discord.Intents.FLAGS.GUILDS,
-    Discord.Intents.FLAGS.GUILD_MEMBERS
-] });
+const client = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MEMBERS
+    ]
+});
+
+const updateMemberCount = () => {
+    const guild = client.guilds.get(config.SERVER_ID);
+    var memberCount = guild.members.filter(member => !member.user.bot).size;
+    var memberCountChannel = client.channels.get("944578541893333022");
+    memberCountChannel.setName(`Member Count: ${memberCount}`);
+    console.log(`Member Count Updated: ${memberCount}`);
+};
+
 
 client.on('ready', () => {
+    updateMemberCount();
     console.log(`Launched as a bot: ${client.user.tag}!`);
 });
 
 /* If user joins server, check if we're looking at the right server, then auto grant "Friends" role. */
 client.on('guildMemberAdd', member => {
-    if(client.guilds.cache.get(config.SERVER_ID)) {
+    if (client.guilds.cache.get(config.SERVER_ID)) {
         return member.roles.add(member.guild.roles.cache.get(config.ROLE_ID));
     }
 });
 
 client.login(config.BOT_TOKEN);
 
-// Test commit
