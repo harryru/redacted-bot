82adaf8e5aa7d80454bce1580051f79d1b2544a3
Edgar Patino <edgar.patino.d@gmail.com>
2022-02-22 22:49:39 -0600

    start of  permission checks

--- redacted-bot/features/commandHandler.js
+++ redacted-bot/features/commandHandler.js
@@ -11,8 +11,35 @@ const commandParser = (args,command) => {
         return;
     } else {
         cmd = command.slice();
-        cmd.splice(0, 1);
-        methods[command[0].toLowerCase()](args,cmd);
+        if(permissionCheck(command[0].toLowerCase())){
+            cmd.splice(0, 1);
+            methods[command[0].toLowerCase()](args,cmd);
+        }else{
+            return;
+        }
+    }
+}
+
+
+const permissionCheck = (args,command) => {
+    var validChannel = false;
+    //var validUser = false;
+    PERMISSIONS[command].channels.map(channel => {
+        if(channel === args.channel.id){
+            validChannel = true;
+        }
+    })
+    console.log(args.member.roles.cache);
+    if(validChannel){
+        return true;
+    }
+}
+
+
+const PERMISSIONS = {
+    invite: {
+        channels: [BOT_CHANNEL_ID],
+        user: [ADMIN_ROLE_ID]
     }
 }
 
