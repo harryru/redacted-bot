c09f42f9005639553a420fbb20a7b4c137b73ff8
Edgar Patino <edgar.patino.d@gmail.com>
2022-02-25 22:27:21 -0600

    test

--- redacted-bot/features/MessageEmbeds.js
+++ redacted-bot/features/MessageEmbeds.js
@@ -4,7 +4,7 @@ const {MessageEmbed} = require('discord.js');
 const constructEmbed = (args) => {
 
     const testEmbed = new MessageEmbed(args)
-        .setDescription(args.content);
+        .setDescription(args.content.slice(8));
 
     args.channel.send({embeds: [testEmbed]});
 }
