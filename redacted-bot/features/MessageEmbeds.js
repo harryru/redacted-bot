a4d45d919d4d5b5160e2bff6f4a2dd07e1c2bdab
Edgar Patino <edgar.patino.d@gmail.com>
2022-02-25 22:19:32 -0600

    update

--- redacted-bot/features/MessageEmbeds.js
+++ redacted-bot/features/MessageEmbeds.js
@@ -4,22 +4,7 @@ const {MessageEmbed} = require('discord.js');
 const constructEmbed = (args) => {
 
     const testEmbed = new MessageEmbed(args)
-        .setColor('#0099ff')
-        .setTitle('Some title')
-        .setURL('https://discord.js.org/')
-        .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-        .setDescription('Some description here')
-        .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-        .addFields(
-            { name: '', value: args.content },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-        )
-        .addField('Inline field title', 'Some value here', true)
-        .setImage('https://i.imgur.com/AfFp7pu.png')
-        .setTimestamp()
-        .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+        .setDescription(args.content);
 
     args.channel.send({embeds: [testEmbed]});
 }
